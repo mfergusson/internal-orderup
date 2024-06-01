e923fbc0ace9b19357c7a68b373e600d9a47a91c
Matt Fergusson <prefix@live.com>
2024-06-01 17:26:05 +0400

    add page

---
+++ src/pages/refund-disputes.tsx
@@ -0,0 +1,9 @@
+import { Typography } from '@mui/material';
+
+const RefundDisputes = () => {
+  return (
+    <Typography variant='h4'>Refund disputes</Typography>
+  )
+}
+
+export default RefundDisputes;
